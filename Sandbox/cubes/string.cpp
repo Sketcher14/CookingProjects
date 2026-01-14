7ac459e9f205c04f35f8db068787c6d3b01e08f2
Vitaliy Glamozda <www.vitaliy_14@mail.ru>
2026-01-14 22:57:38 +0300

    Implemented simple version of the string class using copy & swap idiom for strong exception guarantee

---
+++ Sandbox/cubes/string.cpp
@@ -0,0 +1,135 @@
+#include "cubes/string.h"
+
+#include <cstring>
+#include <print>
+#include <utility>
+
+namespace sb
+{
+string::string(const char* data)
+{
+    if (data == nullptr)
+        return;
+
+    const size_t data_size = strlen(data);
+    _data = new char[data_size + 1];
+
+    strcpy(_data, data);
+    _size = data_size;
+}
+
+string::string(const string& other)
+{
+    if (other._data == nullptr)
+        return;
+
+    _data = new char[other._size + 1];
+    strcpy(_data, other._data);
+    _size = other._size;
+}
+
+string& string::operator=(const string& other)
+{
+    if (this != &other)
+    {
+        string temp(other);
+        swap(*this, temp);
+    }
+
+    return *this;
+}
+
+string::string(string&& other) noexcept
+{
+    swap(*this, other);
+}
+
+string& string::operator=(string&& other) noexcept
+{
+    if (this != &other)
+    {
+        string temp(std::move(other));
+        swap(*this, temp);
+    }
+
+    return *this;
+}
+
+string::~string()
+{
+    delete[] _data;
+}
+
+void swap(string& left, string& right) noexcept
+{
+    std::swap(left._size, right._size);
+    std::swap(left._data, right._data);
+}
+
+char& string::operator[](const size_t index)
+{
+    return _data[index];
+}
+
+const char& string::operator[](const size_t index) const
+{
+    return _data[index];
+}
+
+std::strong_ordering string::operator<=>(const string& other) const noexcept
+{
+    if (_data == nullptr && other._data == nullptr)
+        return std::strong_ordering::equal;
+    if (_data == nullptr)
+        return std::strong_ordering::less;
+    if (other._data == nullptr)
+        return std::strong_ordering::greater;
+
+    const int result = strcmp(_data, other._data);
+    if (result < 0)
+        return std::strong_ordering::less;
+    if (result > 0)
+        return std::strong_ordering::greater;
+
+    return std::strong_ordering::equal;
+}
+
+bool string::operator==(const string& other) const noexcept
+{
+    if (_data == nullptr && other._data == nullptr)
+        return true;
+    if (_data == nullptr || other._data == nullptr)
+        return false;
+
+    if (_size != other._size)
+        return false;
+
+    return strcmp(_data, other._data) == 0;
+}
+
+size_t string::size() const noexcept
+{
+    return _size;
+}
+
+bool string::empty() const noexcept
+{
+    return _size == 0;
+}
+
+const char* string::data() const noexcept
+{
+    return _data == nullptr ? "" : _data;
+}
+
+void string::print() const noexcept
+{
+    if (_data == nullptr)
+        return;
+
+    for (size_t i = 0; i < _size; ++i)
+        std::print("{}", _data[i]);
+
+    std::println();
+}
+}
---
+++ Sandbox/cubes/string.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <compare>
+#include <cstddef>
+
+namespace sb
+{
+class string final
+{
+public:
+    string() = default;
+    explicit string(const char* data);
+
+    string(const string& other);
+    string& operator=(const string& other);
+
+    string(string&& other) noexcept;
+    string& operator=(string&& other) noexcept;
+
+    ~string();
+
+    friend void swap(string& left, string& right) noexcept;
+
+    [[nodiscard]] char& operator[](const size_t index);
+    [[nodiscard]] const char& operator[](const size_t index) const;
+    [[nodiscard]] std::strong_ordering operator<=>(const string& other) const noexcept;
+    [[nodiscard]] bool operator==(const string& other) const noexcept;
+
+    [[nodiscard]] size_t size() const noexcept;
+    [[nodiscard]] bool empty() const noexcept;
+    [[nodiscard]] const char* data() const noexcept;
+
+    void print() const noexcept;
+
+private:
+    char* _data = nullptr;
+    size_t _size = 0;
+};
+}
