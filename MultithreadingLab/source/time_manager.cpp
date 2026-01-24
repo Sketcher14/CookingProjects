cf1979e20e39714079af4badbcb1e0d3e88c9a58
Vitaliy Glamozda <www.vitaliy_14@mail.ru>
2026-01-24 23:11:27 +0300

    Implemented multithread version of the time manager class with simple functionality

---
+++ MultithreadingLab/source/time_manager.cpp
@@ -0,0 +1,55 @@
+#include "time_manager.h"
+
+time_manager::time_manager()
+{
+    _thread = std::jthread(std::bind_front(&time_manager::run, this));
+}
+
+time_manager::~time_manager()
+{
+    _thread.request_stop();
+
+    if (_thread.joinable())
+        _thread.join();
+}
+
+void time_manager::set_timer(std::function<void()> callback, const time_point fire_time)
+{
+    bool notify = false;
+    {
+        std::scoped_lock lock(_mutex);
+        notify = _callbacks.empty();
+        _callbacks.emplace(std::move(callback), fire_time);
+    }
+
+    if (notify)
+        _cv.notify_one();
+}
+
+void time_manager::run(const std::stop_token stop_token)
+{
+    while (true)
+    {
+        std::unique_lock lock(_mutex);
+
+        const time_point top_fire_time = _callbacks.empty() ? time_point::max() : _callbacks.top().fire_time;
+
+        _cv.wait_until(lock, stop_token, top_fire_time, [this]() { return !_callbacks.empty(); });
+
+        if (stop_token.stop_requested())
+            break;
+
+        while (!_callbacks.empty())
+        {
+            const time_point next_fire_time = _callbacks.top().fire_time;
+
+            if (std::chrono::system_clock::now() >= next_fire_time)
+            {
+                auto callback = _callbacks.top().callback;
+                _callbacks.pop();
+
+                callback();
+            }
+        }
+    }
+}
---
+++ MultithreadingLab/source/time_manager.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <queue>
+#include <thread>
+
+
+class time_manager final
+{
+    using time_point = std::chrono::time_point<std::chrono::system_clock>;
+
+public:
+    time_manager();
+    ~time_manager();
+
+    void set_timer(std::function<void()> callback, const time_point fire_time);
+
+private:
+    struct callback_data
+    {
+        std::function<void()> callback;
+        time_point fire_time;
+
+        explicit callback_data(std::function<void()> in_callback, const time_point in_fire_time) :
+            callback(std::move(in_callback)), fire_time(in_fire_time)
+        {}
+
+        bool operator<(const callback_data& other) const
+        {
+            return fire_time > other.fire_time;
+        }
+    };
+
+    std::jthread _thread;
+    std::recursive_mutex _mutex;
+    std::condition_variable_any _cv;
+
+    std::priority_queue<callback_data> _callbacks;
+
+    void run(const std::stop_token stop_token);
+};
